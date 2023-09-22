6f0da2b69d8d6779669c2a0913006f60f625a3a4
Cody Hergenroeder <codyhergenroeder@stanford.edu>
2023-09-22 00:36:40 -0700

    rebuilt sim from scratch.mit.edu version

--- simulation.py
+++ simulation.py
@@ -1,194 +1,134 @@
+import math
 import random
+from typing import Optional
+# 1 tick is 1 day
 
-from pywebio.input import *
-from pywebio.output import *
+# uses statistics to determine whether or not a particular event happens this year
+# in: x, mu, sigma
+def event_happens(x: int, mu: int, sigma: float = 10) -> bool:
+    return random.random() <= (1 / (sigma * math.sqrt(2 * math.pi))) * math.exp(-0.5 * ((x - mu) / sigma)**2)
 
-# from items import *
-
-def rand_from_list(list):
-	return list[random.randrange(len(list))]
-
-def rand_bool():
-	return bool(random.getrandbits(1))
-
-# current dna works by choosing randomly between the *phenotypes* of your parents
-# eventually dna will store genotypes
-def recombine_dna(dna):
-	combined_dna = {}
-	for key, value in dna.items():
-		combined_dna[key] = rand_from_list(value)
-	return combined_dna
-
-# this function handles the front-end
-def output_text(text):
-	print(text)
-	put_text(text)
-
-def print_humans(human_list):
-	table = []
-	for num, human in enumerate(human_list):
-		# add header row
-		if num == 0:
-			table += [vars(human).keys()]
-		# vars() is a nifty func that takes an object's attributes and prints them
-		table += [vars(human).values()]
-	put_table(table)
-
-# outputs current state of sim at the end of a tick
-def sim_output(tick, humans):
-	output_text(f"\nTick: {tick}")
-	print_humans(humans)
-
-class Human():
-	def __init__(self, name="", birthdate=0, height=0, food_need = 2, dna={'sex' : '', 'temperament': {}, 'height': 0}, parents=[]):
-		self.name = name
-		self.birthdate = birthdate
-		self.dna = dna
-		self.height = height
-		self.food_need = food_need
-		self.parents = parents
-
-		self.inventory = dict()
-		self.children = list()
-	
-	def create_child(self, parent2, curr_tick):
-		if(self.dna['sex'] != parent2.dna['sex']):
-			# genetic stuff
-			name = self.name + parent2.name
-			split_dna = {'sex': [self.dna['sex'], parent2.dna['sex']], 'temperament': [self.dna['temperament'], parent2.dna['temperament']], 'height': [self.dna['height'], parent2.dna['height']]}
-			dna = recombine_dna(split_dna)
-
-			# linked-list housekeeping (want to be able to navigate the tree of humans)
-			child = Human(name=name, birthdate=curr_tick, dna=dna, parents=[self, parent2])
-			self.children.append(child)
-			parent2.children.append(child)
-			return child
-		return None  # abort func
-
-	# this function shows you a human
-	# stand-in for future dataviz pop-up, or a more complex UI
-	def printme(self, tick):
-		print_humans([self])
-
-	# this func dynamically calculates age as-needed so we dont have to manually update per tick
-	def get_age(self, curr_tick):
-		return curr_tick - self.birthdate
-
-class Planet():
-	day_night_time = (14, 10)  # for now, 1 day = 1 tick
-	time_per_lemon = 2
-
-
+# has: list of human objs, 
 class Simulation():
-	# initialize the sim		
-	def __init__(self, ticks_to_run=0, num_init_humans=0, humans=[]):
+	def __init__(self, time_elapsed_ticks=0, ticks_left_before_stop=0, humans=[]):
+		self.time_elapsed_ticks = 0
+		self.ticks_left_before_stop = ticks_left_before_stop
 		self.humans = humans
-		self.num_init_humans = num_init_humans
-		self.curr_tick = 0
+		self.LIFE_EXPECTANCY = random.randrange(45,60)
 
-		planet = Planet()
-		self.make_starter_humans(num_init_humans)
-		while ticks_to_run != 0:
-			self.simulate(ticks_to_run)
-			ticks = input('Ticks to run:', type=NUMBER)
+		self.sim_messages:list[str] = []
 
+		self.births_this_tick = 0
+		self.deaths_this_tick = 0
+		self.pubescenses_this_tick = 0
+		self.marriages_this_tick = 0
 
-	def make_starter_humans(self, num_humans):
-		# create the specified # of humans, w random attributes
-		for i in range(int(num_humans)):
-			is_male = rand_bool() 
-			name, sex = ("Adam", "M") if is_male else ("Eve", "F")
-			new_human = Human(name=name, height=6, birthdate=-25, dna={'sex':sex, 'temperament':'', 'height': 6})
-			self.humans.append(new_human)
+		# create seed humans
+		for i in range(random.randrange(15,40)):
+			self.humans.append(Human(age=0, gender=random.choices([0,1,2],[0.45,0.45,0.1], k=1)[0], is_alive_bool = True, puberty_bool=False, parents=None, children=None, spouse=None, job=None))
+		self.sim_messages.append(f'You come into awareness of a corner of the world that has {len(self.humans)} humans in it')
 
-		# make a bunch of babies as a debug
-		for a in self.humans:
-			if a.name == 'Adam':
-				for e in self.humans:
-					if e.name == 'Eve':	
-						baby = a.create_child(e, self.curr_tick)
-						self.humans.append(baby)
-		return
+	def do_tick(self):
+		self.births_this_tick = 0
+		self.deaths_this_tick = 0
+		self.pubescenses_this_tick = 0
+		self.marriages_this_tick = 0
 
 
-	# this function does the dirty work of the sim
-	# 1 tick = 1 day
-	def simulate(self, ticks = 1):
-		if ticks >= 1:
-			for i in range(ticks):
-				# update states
-				self.age_humans(self.humans)
-				# simulate decisions
-				self.forage(self.humans)
-				# check on states
-				self.check_nutrition(self.humans)
-				# wrap up the tick and output relevant data
-				self.curr_tick += 1
-				sim_output(self.curr_tick, self.humans)
-
-	# increase everyones age and height
-	# O(n)
-	def age_humans(self, humans):
-		for human in human:
-			age = human.get_age(self.curr_tick)
-			# all humans grow linearly towards their genetic max at 18 y.o.
-			if age < 18:	
-				age_delta = 18 - human.get_age(self.curr_tick)
-				height_delta = human.dna['height'] - human.height
-				human.height += height_delta/age_delta
-
-	# arguments: a human, an object string, and a negative/positive quantity to change
-	# manipulates teh human's inventory, and returns False if human had insufficient items when quantity negative
-	# if using a large quantity, consider calling the function in a loop if returning as False is unwanted behavior
-	def edit_inventory(self, human, object, quantity=1):
-		obj_check = human.inventory.get(object)  # should be None if not present, or the quantity of the object in inventory
-		if quantity < 1:
-			if obj_check is None or not obj_check - quantity >= 0:
-				# insufficient items for check
-				sufficient_quantity = False
-			else:
-				human.inventory[object] += quantity  # quantity is negative
-				sufficient_quantity = True
-		elif quantity > 1:
-			sufficient_quantity = True
-			# use .get to initialize inventory quantity of object to 0 then add quantity
-			human.inventory[object] = human.inventory.get(object,0) + quantity
-		# remove item from inventory if none of it is left!
-		# this may be inefficient, but makes for a clearer display
-		if human.inventory.get(object) == 0:
-			del human.inventory[object]
-		return sufficient_quantity
-
-	# every human forages 2 lemons per day
-	# TODO only adults? + who forages lemons? 
-	# O(n)
-	def forage(self, humans):
-		for human in humans:
-			self.edit_inventory(human, 'lemon', 3)
-		pass
-
-	# TODO manage dead humans (put in separate list? then how will i handle parent/child checking?)
-	def kill_human(self, human, reason="unknown causes"):
-		output_text(f"{human.name} died of {reason}... but we gave them a second chance!")
-		pass
-
-	# calculate whether each human has enough food to survive
-	# TODO: familial systems must provide lemons to their children
-	# O(n)
-	def check_nutrition(self, humans):
+		# birth children
+		# find married (living) women
+		# (would it be cheaperr to have a re-usable 'list of married women')? deal with deaths/remarriages also
 		for human in self.humans:
-			for i in range(human.food_need):
-				if self.edit_inventory(human, 'lemon', -1):
-					output_text(f"{human.name} ate a lemon.")
-				else:
-					# if youre even one lemon too short, youre not gonna make it...
-					self.kill_human(human, reason="starvation")
-		pass
+			# if eligible for birthing (married, all partners alive)
+			if human.gender==0 and human.spouse and human.is_alive_bool and human.spouse.is_alive_bool:
+				# likelihood of pregnancy in a married couple
+				if random.random() > 0.3:
+					self.humans.append(human.give_birth(human.spouse))
+					self.births_this_tick += 1
+
+		# age out humans
+		for human in self.humans:
+			# determine whether human dies
+			# normal distribution around LIFE_EXPECTANCY
+			if event_happens(human.age, self.LIFE_EXPECTANCY, 5):
+				human.is_alive_bool = False
+				self.deaths_this_tick += 1
+
+			if human.is_alive_bool:
+				human.age += 1
+
+		# determine puberty
+		for human in self.humans:
+			if not human.puberty_bool:
+				if (human.gender == 0 and event_happens(human.age, 10, 1.5)) or (human.gender == 1 and event_happens(human.age, 12, 1.5)):
+					human.puberty_bool = True
+					self.pubescenses_this_tick += 1
+
+		# pair spouses (O(n)!)
+		# TODO more efficient way to hide dead humans from list to prevent iterating. how? do i move to a separate list?
+		for human in self.humans:
+			# female-first matching; will soon have an alt approach
+			if human.gender==0 and not human.spouse and human.is_alive_bool:
+				if random.random() <= 0.3:
+					# pick random living human of appropriate gender
+					tries = 0
+					rand_human = None
+					# matching is currently picking a random human
+					while(not rand_human or rand_human.gender!=1 or rand_human.spouse != None or not rand_human.is_alive_bool):					
+						if (tries > 256):
+							rand_human = None
+							break
+						rand_human = random.choice(self.humans)
+						tries += 1
+					# if successful match was found
+					if rand_human:
+						human.spouse = rand_human
+						rand_human.spouse = human 
+						self.marriages_this_tick += 1
+
+		# show readout
+		self.print_sim_readout()
+
+		# mark tick completed, raise counter
+		self.time_elapsed_ticks += 1
+		print('Simulated 1 tick.')
+
+	def print_sim_readout(self):
+		while self.sim_messages:
+			print(self.sim_messages[0])
+			self.sim_messages.pop(0)
+		print(f'TICK {self.time_elapsed_ticks}:')
+		print(f'Total Humans: {len(self.humans)}')
+
+		print(f'Births: {self.births_this_tick}')
+		print(f'Marriages: {self.marriages_this_tick}')
+		# print(f'Pubescences: ${self.pubescenses_this_tick}')
+		print(f'Deaths: {self.deaths_this_tick}')
 
 
-if __name__ == "__main__":
-	ticks = input('Ticks to run:', type=NUMBER)
-	humans = input('Initial Humans:', type=NUMBER)
 
-	sim = Simulation(ticks, humans)
+class Human:
+	def __init__(self, age:int, gender:int, is_alive_bool:bool, puberty_bool:bool, parents:Optional[list["Human"]], children:Optional[list["Human"]], spouse:Optional["Human"], job:Optional[str]):
+		self.age = age
+		self.gender = gender 
+		self.is_alive_bool = is_alive_bool
+		self.puberty_bool = puberty_bool
+		self.parents= parents
+		self.children = children
+		self.spouse = spouse
+		self.job = job
 
+	# combine self and partner genetics into a new human
+	def give_birth(self, partner) -> "Human":
+		return Human(age=0, gender=random.choices([0,1,2],[0.45,0.45,0.1], k=1)[0], is_alive_bool = True, puberty_bool=False, parents=[self, partner], children=None, spouse=None, job=None)
+
+
+
+if __name__ == '__main__':
+	ticks_left_before_stop = int(input('Welcome to your world simulation! How many ticks would you like to simulaculate before stopping? Eg. 10:\n'))
+	sim = Simulation(0, ticks_left_before_stop)
+
+	while(True):
+		while(sim.time_elapsed_ticks < sim.ticks_left_before_stop ):
+			sim.do_tick()
+		sim.ticks_left_before_stop = int(input('How many ticks would you like to simulaculate before stopping? Eg. 10:\n')) + sim.time_elapsed_ticks
